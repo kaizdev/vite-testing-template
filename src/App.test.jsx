279b7a128066ee4190b32581016e246e8ee3d215
Kai Zhang <kai.zhang@outlook.com.au>
2023-07-06 15:00:20 +1000

    first commmit - adds vite-testing-template

---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+// Testing the testing environment is set up correctly
+// run with npm run test
+
+describe("Setup test", () => {
+    it("checks true", () => {
+        expect(true).toBe(true);
+    });
+});
